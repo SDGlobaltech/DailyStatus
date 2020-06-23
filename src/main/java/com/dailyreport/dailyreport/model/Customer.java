fbdfe39af0ec2149f64e3c2c3586ab14f145230a
sdglobaltech <admin@example.com>
2020-06-23 22:25:11 +0800

    Daily Status Report

--- src/main/java/com/dailyreport/dailyreport/model/Customer.java
+++ src/main/java/com/dailyreport/dailyreport/model/Customer.java
@@ -12,45 +12,48 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "customer")
 public class Customer implements Serializable {
- 
+
 	private static final long serialVersionUID = -2343243243242432341L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
- 
+
 	@Column(name = "firstname")
 	private String firstName;
- 
+
 	@Column(name = "lastname")
 	private String lastName;
-	
+
 	@Column(name = "CreationDate")
 	private String Date;
-	
+
 	@Column(name = "status_date")
 	private String statusDate;
-	
+
 	@Column(name = "clientname")
 	private String clientName;
 	@Column(name = "stats_date")
 	private String statsDate;
- 
+
 	protected Customer() {
 	}
- 
-	public Customer(String firstName, String lastName, String date, String statusDate, String clientName,String statsDate  ) {
+
+	public Customer(String firstName, String lastName, String date, String statusDate, String clientName,
+			String statsDate) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.Date = date;
-		this.statusDate= statusDate;
-		this.clientName=clientName;
-		this.statsDate=statsDate;
-		
+		this.statusDate = statusDate;
+		this.clientName = clientName;
+		this.statsDate = statsDate;
+
 	}
- 
+
 	@Override
 	public String toString() {
-		return String.format("Customer[id=%d, firstName='%s', lastName='%s', Date='%s',statusDate='%s',clientName='%s',statsDate='%s']", id, firstName, lastName, Date,statusDate,clientName,statsDate);
+		return String.format(
+				"Customer[id=%d, firstName='%s', lastName='%s', Date='%s',statusDate='%s',clientName='%s',statsDate='%s']",
+				id, firstName, lastName, Date, statusDate, clientName, statsDate);
 	}
 
 	public String getFirstName() {
@@ -101,32 +104,4 @@ public class Customer implements Serializable {
 		this.statsDate = statsDate;
 	}
 
-
-
-
-
-
-	
-	
-	
-//	public String getFirstName() {
-//        return firstName;
-//    }
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//    public String getLastName() {
-//        return lastName;
-//    }
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//    public String getDate(String date) {
-//        return Date;
-//    }
-//    public void setDate(String date) {
-//        this.Date = date;
-//    }
-//    
-    
 }
