af2430a8a17131daf22af0a082d6764afb67d34b
sdglobaltech <admin@example.com>
2020-06-23 23:01:15 +0800

    Log file creation

--- src/main/java/com/dailyreport/dailyreport/DailyreportApplication.java
+++ src/main/java/com/dailyreport/dailyreport/DailyreportApplication.java
@@ -1,15 +1,21 @@
 package com.dailyreport.dailyreport;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DailyreportApplication {
+	private static final Logger LOGGER = Logger.getAnonymousLogger();
 
 	public static void main(String[] args) {
+		System.out.println("Current Directory = " + System.getProperty("user.dir"));
 		SpringApplication.run(DailyreportApplication.class, args);
-		
+
 		System.out.println("this is ramesh chowdary");
+		LOGGER.info("just a test info log");
 	}
 
 }
--- src/main/java/com/dailyreport/dailyreport/controller/dailyReportController.java
+++ src/main/java/com/dailyreport/dailyreport/controller/dailyReportController.java
@@ -3,8 +3,10 @@ package com.dailyreport.dailyreport.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,36 +34,36 @@ public class dailyReportController {
 //	                         , new Customer("Dharmendra", "Bhojwani")));
 //	      return "Customers are created";
 //	  }
-	
-	 @PostMapping("/create")
-	  public String create(@RequestBody List<CustomerUI>  customer){
-	      // save a single Customer
-	     for (CustomerUI customerUI : customer) {
+
+	@PostMapping("/create")
+	public String create(@RequestBody List<CustomerUI> customer) {
+		// save a single Customer
+		for (CustomerUI customerUI : customer) {
 			/*
 			 * String firstName=customerUI.getFirstName(); String
 			 * lastName=customerUI.getLastName(); String date=customerUI.getdate();
 			 */
-			Customer customerSave=repository.save(new Customer(customerUI.getFirstName(), customerUI.getLastName(),customerUI.getDate(),customerUI.getStatusDate(),customerUI.getClientName(),customerUI.getStatsDate()));
-			
+			Customer customerSave = repository
+					.save(new Customer(customerUI.getFirstName(), customerUI.getLastName(), customerUI.getDate(),
+							customerUI.getStatusDate(), customerUI.getClientName(), customerUI.getStatsDate()));
+
 		}
-		 
-	      return "Data inserted into postgres DB";
-	  
-	      
-	  }
-	
-	 
-	 @GetMapping("/findall")
-	  public List<CustomerUI> findAll(){
-	      List<Customer> customers = repository.findAll();
-	      List<CustomerUI> customerUI = new ArrayList<>();
-	      for (Customer customer : customers) {
-	      customerUI.add(new CustomerUI(customer.getFirstName(),customer.getLastName(),customer.getDate(),customer.getStatusDate(),customer.getClientName(),customer.getStatsDate()));
-	      }
-	      return customerUI;
-	  }
-	 
-	 
+
+		return "Data inserted into postgres DB";
+
+	}
+
+	@GetMapping("/findall")
+	public List<CustomerUI> findAll() {
+		List<Customer> customers = repository.findAll();
+		List<CustomerUI> customerUI = new ArrayList<>();
+		for (Customer customer : customers) {
+			customerUI.add(new CustomerUI(customer.getFirstName(), customer.getLastName(), customer.getDate(),
+					customer.getStatusDate(), customer.getClientName(), customer.getStatsDate()));
+		}
+		return customerUI;
+	}
+
 //	  @RequestMapping("/search/{id}")
 //	  public String search(@PathVariable long id){
 //	      String customer = "";
